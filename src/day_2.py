0408742de67b3333c47a316f05be74cdbb173951
aidenywl <aidenlow@u.nus.edu>
2023-12-01 21:24:54 -0800

    day 2 part 2

--- src/day_2.py
+++ src/day_2.py
@@ -8,6 +8,9 @@ treb7uchet -> 77
 
 import re
 import helper
+from collections import defaultdict
+import numpy
+
 def is_subset_possible(subset: list[str]) -> bool:
     cube_count_map = {
         "red": 12,
@@ -44,6 +47,40 @@ def part_one(data: list[str]) -> int:
             res += game_id
     return res
 
+CUBES = ['red', 'green', 'blue']
+"""
+Part 2
+"""
+def get_min_cubes_required(subset: list[str]) -> defaultdict[int]:
+    min_cubes_required = defaultdict(int)
+    for s in subset:
+        play = s.split(" ")
+        num, color  = int(play[0]), play[1]
+        min_cubes_required[color] += num
+    # check if any are negative
+    return min_cubes_required
+
+
+def power_of_game(line: str) -> (bool, int):
+    res = defaultdict(int)
+    game_metadata = [s.strip() for s in line.split(":")]
+    game_subsets = [g.strip() for g in game_metadata[1].split(";")]
+    for subset in game_subsets:
+        subset_list = [s.strip() for s in subset.split(",")]
+        min_cube_map = get_min_cubes_required(subset_list)
+        for key in CUBES:
+            res[key] = max(res[key], min_cube_map[key])
+    num = list(res.values())
+    return numpy.prod(num)
+
+
+def part_two(data: list[str]) -> int:
+    res = 0
+    for line in data:
+        val = power_of_game(line)
+        res += val
+    return res
+
 if __name__ == "__main__":
     # read input
     input_file = open(helper.get_input_data_path(2))
@@ -51,4 +88,6 @@ if __name__ == "__main__":
     calibration_data = input_file.readlines()
     res = part_one(calibration_data)
     print("Part 1 answer: ", res)
+    res = part_two(calibration_data)
+    print("Part 2 answer: ", res)
 
